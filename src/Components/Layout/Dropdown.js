dedecd179517b51ae395ddd08277635bce86fe53
Blazej Duszynski <bduszynski92@gmail.com>
2023-03-09 21:36:38 +0100

    added seasons available in dropdown and styled list items

--- src/Components/Layout/Dropdown.js
+++ src/Components/Layout/Dropdown.js
@@ -9,16 +9,34 @@ const Dropdown = () => {
     setIsDropDownActive(!isDropdownActive);
   };
 
+  const seasons = [
+    "2008/2009",
+    "2009/2010",
+    "2010/2011",
+    "2011/2012",
+    "2012/2013",
+    "2013/2014",
+    "2014/2015",
+    "2015/2016",
+    "2016/2017",
+    "2017/2018",
+    "2018/2019",
+    "2019/2020",
+    "2020/2021",
+  ];
+
   return (
     <div className={classes.dropdown}>
       <div className={classes.select} onClick={activateDropdownHandler}>
         <span></span>
         <ArrowDropDownIcon
-          className={isDropdownActive && classes.arrowIconRotated}
+          className={isDropdownActive? classes.arrowIconRotated : classes.arrowIcon}
         />
       </div>
       <ul className={isDropdownActive ? classes.menu__active : classes.menu}>
-        <li>sdas</li>
+        {seasons.map((season) => {
+          return <li className={classes.menuItem}>{season}</li>;
+        })}
       </ul>
     </div>
   );
