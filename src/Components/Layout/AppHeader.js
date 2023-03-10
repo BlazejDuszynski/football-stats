64bc7a2d826e77a4e4b4eceb38c58bcf44e1f7cb
Blazej Duszynski <bduszynski92@gmail.com>
2023-03-10 22:39:23 +0100

    changed button color and layout

--- src/Components/Layout/AppHeader.js
+++ src/Components/Layout/AppHeader.js
@@ -5,10 +5,9 @@ function AppHeader(props) {
     <div className={classes.appHeader}>
       <div className={classes.logo}></div>
       <div className={classes.text}>
-        <h1>Welcome to Manchester United Football Club stats center!</h1>
+        <h1>Manchester United Football Club stats center</h1>
         <h2>
-          Find the most accurate players statistics across Premier League
-          seasons from 2008/2009 to 2020/2021.
+          Find the most accurate players statistics from Premier League!
         </h2>
       </div>
     </div>
