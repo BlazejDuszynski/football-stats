ff25d04e6f555810c16f580878c2c2dd5d35b7cc
Blazej Duszynski <bduszynski92@gmail.com>
2023-03-18 14:50:24 +0100

    fixed height and width of player picture

--- src/Components/Layout/Player/PlayerWindowHeader.js
+++ src/Components/Layout/Player/PlayerWindowHeader.js
@@ -5,7 +5,11 @@ function PlayerWindowHeader(props) {
   return (
     <header className={classes.header}>
       <div className={classes.intro}>
-        <div className={classes.photo}></div>
+        <img
+          src="https://img.a.transfermarkt.technology/portrait/header/3396-1570441538.jpg?lm=1"
+          alt="Roy Keane face"
+          className={classes.photo}
+        ></img>
         <div className={classes.name}>
           <p className={classes.name__first}>Roy</p>
           <p className={classes.name__second}>Keane</p>
