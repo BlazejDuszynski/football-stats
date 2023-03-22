a4416ae9045798f1bd9f3f5b81e1a0a6eea5fbdb
Blazej Duszynski <bduszynski92@gmail.com>
2023-03-22 20:06:04 +0100

    removed on hover effects from charts

--- src/Components/Layout/Player/Stats/Charts/PieChart.js
+++ src/Components/Layout/Player/Stats/Charts/PieChart.js
@@ -13,7 +13,10 @@ const PieChart = () => {
         label: "Starting 11 chart",
         data: [76, 24],
         backgroundColor: ["#4776E6", "#1c1b36"],
+        hoverBackgroundColor: ["#4776E6", "#1c1b36"],
         borderColor: ["#4776E6", "#1c1b36"],
+        hoverBorderColor: ["#4776E6", "#1c1b36"],
+        borderWidth: 0,
       },
     ],
   };
@@ -23,6 +26,13 @@ const PieChart = () => {
       legend: {
         display: false,
       },
+      tooltip: {
+        enabled: false,
+      },
+    },
+    interaction: {
+      mode: "index",
+      intersect: false,
     },
   };
   return (
