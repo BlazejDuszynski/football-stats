8e34ca679d790bfc414c43fb0cf6d26c61263f14
Blazej Duszynski <bduszynski92@gmail.com>
2023-03-22 20:25:49 +0100

    dynamically rendered pieChartSummaryNumber inside of PieChart

--- src/Components/Layout/Player/Stats/Charts/ChartContainer.js
+++ src/Components/Layout/Player/Stats/Charts/ChartContainer.js
@@ -3,11 +3,70 @@ import classes from "./ChartContainer.module.css";
 import PieChart from "./PieChart";
 
 const ChartContainer = () => {
+  const dataFirst = {
+    labels: ["Starting 11", "Bench"],
+    datasets: [
+      {
+        label: "Starting 11 chart",
+        data: [76, 24],
+        backgroundColor: ["#4776E6", "#1c1b36"],
+        hoverBackgroundColor: ["#4776E6", "#1c1b36"],
+        borderColor: ["#4776E6", "#1c1b36"],
+        hoverBorderColor: ["#4776E6", "#1c1b36"],
+        borderWidth: 0,
+      },
+    ],
+  };
+
+  const dataSecond = {
+    labels: ["Goals involvement", ""],
+    datasets: [
+      {
+        label: "Starting 11 chart",
+        data: [38, 62],
+        backgroundColor: ["#4776E6", "#1c1b36"],
+        hoverBackgroundColor: ["#4776E6", "#1c1b36"],
+        borderColor: ["#4776E6", "#1c1b36"],
+        hoverBorderColor: ["#4776E6", "#1c1b36"],
+        borderWidth: 0,
+      },
+    ],
+  };
+
+  const dataThird = {
+    labels: ["Minutes played", ""],
+    datasets: [
+      {
+        label: "Starting 11 chart",
+        data: [88, 12],
+        backgroundColor: ["#4776E6", "#1c1b36"],
+        hoverBackgroundColor: ["#4776E6", "#1c1b36"],
+        borderColor: ["#4776E6", "#1c1b36"],
+        hoverBorderColor: ["#4776E6", "#1c1b36"],
+        borderWidth: 0,
+      },
+    ],
+  };
+
+  const options = {
+    plugins: {
+      legend: {
+        display: false,
+      },
+      tooltip: {
+        enabled: false,
+      },
+    },
+    interaction: {
+      mode: "index",
+      intersect: false,
+    },
+  };
   return (
     <div className={classes.chartContainer}>
-      <PieChart />
-      <PieChart />
-      <PieChart />
+      <PieChart data={dataThird} options={options} />
+      <PieChart data={dataFirst} options={options} />
+      <PieChart data={dataSecond} options={options} />
     </div>
   );
 };
--- src/Components/Layout/Player/Stats/Charts/PieChart.js
+++ src/Components/Layout/Player/Stats/Charts/PieChart.js
@@ -5,40 +5,21 @@ import { Doughnut } from "react-chartjs-2";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-const PieChart = () => {
-  const data = {
-    labels: ["Starting 11", "Bench"],
-    datasets: [
-      {
-        label: "Starting 11 chart",
-        data: [76, 24],
-        backgroundColor: ["#4776E6", "#1c1b36"],
-        hoverBackgroundColor: ["#4776E6", "#1c1b36"],
-        borderColor: ["#4776E6", "#1c1b36"],
-        hoverBorderColor: ["#4776E6", "#1c1b36"],
-        borderWidth: 0,
-      },
-    ],
-  };
+const PieChart = (props) => {
+  const dataSum = props.data.datasets[0].data.reduce(
+    (partialSum, a) => partialSum + a,
+    0
+  );
 
-  const options = {
-    plugins: {
-      legend: {
-        display: false,
-      },
-      tooltip: {
-        enabled: false,
-      },
-    },
-    interaction: {
-      mode: "index",
-      intersect: false,
-    },
-  };
+  const summaryNumber = (props.data.datasets[0].data[0] / dataSum) * 100;
+  const summaryNumberPercentage = summaryNumber + "%";
+
+  console.log(props.data.datasets[0].data);
+
   return (
     <div className={classes.pieChartContainer}>
-      <Doughnut data={data} options={options}></Doughnut>
-      <p className={classes.pieChartSummaryNumber}>76%</p>
+      <Doughnut data={props.data} options={props.options}></Doughnut>
+      <p className={classes.pieChartSummaryNumber}>{summaryNumberPercentage}</p>
       <label className={classes.pieChartLabel}>Starting 11</label>
     </div>
   );
