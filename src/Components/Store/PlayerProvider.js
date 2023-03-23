7f84fbfcc912bd7fc6926ca18926338b9c572eb8
Blazej Duszynski <bduszynski92@gmail.com>
2023-03-23 18:18:49 +0100

    added PlayerID and getPlayerIDHandler to PlayerContext

--- src/Components/Store/PlayerProvider.js
+++ src/Components/Store/PlayerProvider.js
@@ -3,16 +3,31 @@ import PlayerContext from "./player-context";
 import SquadContext from "./squad-context";
 
 const PlayerProvider = (props) => {
+  const [playerID, setPlayerID] = useState(null);
   const [playerData, setPlayerData] = useState(null);
   const { season } = useContext(SquadContext);
 
   console.log(season);
 
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "72cff716cdmshc41548afe41ba07p18c95cjsn9521d9d88440",
+      "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com",
+    },
+  };
+
   const fetchPlayerDataHandler = () => {};
 
+  const getPlayerIDHandler = (id) => {
+    setPlayerID(id);
+  };
+
   const playerContext = {
-    player: {},
+    playerData: {},
     fetchPlayerData: fetchPlayerDataHandler,
+    playerID: playerID,
+    getPlayerID: getPlayerIDHandler,
   };
 
   return (
--- src/Components/Store/player-context.js
+++ src/Components/Store/player-context.js
@@ -3,6 +3,8 @@ import React from "react";
 const PlayerContext = React.createContext({
   player: {},
   fetchPlayerData: () => {},
+  playerID: "",
+  getPlayerID: () => {},
 });
 
 export default PlayerContext;
