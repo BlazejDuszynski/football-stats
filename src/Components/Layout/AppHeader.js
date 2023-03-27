b573eeb761f8a4d9c67c9e9ce749e90b0c71e407
Blazej Duszynski <bduszynski92@gmail.com>
2023-03-27 20:25:03 +0200

    removed premier league logo from AppHeader

--- src/Components/Layout/AppHeader.js
+++ src/Components/Layout/AppHeader.js
@@ -3,7 +3,6 @@ import classes from "./AppHeader.module.css";
 function AppHeader(props) {
   return (
     <div className={classes.appHeader}>
-      <div className={classes.logo}></div>
       <div className={classes.text}>
         <h1>Welcome to Manchester United Football Club stats center</h1>
         <h2>
--- src/Components/Layout/LandingPage.js
+++ src/Components/Layout/LandingPage.js
@@ -4,10 +4,17 @@ import classes from "./LandingPage.module.css";
 function LandingPage() {
   return (
     <div className={classes.landingPage}>
+      {/* <nav className={classes.navbar}>
+        <a className={classes.navbarItem}></a>
+        <a className={classes.navbarItem}></a>
+      </nav> */}
       <div className={classes.content}>
-        <h1 className={classes.mainHeader}>Application with football statistics</h1>
+        <h1 className={classes.mainHeader}>
+          Application with football statistics
+        </h1>
         <h2 className={classes.secondaryHeader}>
-          Find the most accurate statistics from English Premier League from seasons 2008/2009 - 2020/2021 including played games, goals, assists!
+          Find the most accurate statistics from English Premier League from
+          seasons 2008/2009 - 2020/2021 including played games, goals, assists!
         </h2>
         <button className={classes.getStartedButton}>Get started</button>
       </div>
